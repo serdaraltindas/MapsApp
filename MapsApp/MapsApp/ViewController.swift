89c1851b32e82e0dc21acc2f3e1c5c56ae9932f7
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-23 22:56:07 +0300

    MapKit, CoreLocation

--- MapsApp/MapsApp/ViewController.swift
+++ MapsApp/MapsApp/ViewController.swift
@@ -1,19 +1,26 @@
-//
-//  ViewController.swift
-//  MapsApp
-//
-//  Created by Serdar Altındaş on 23.04.2023.
-//
-
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapKit: MKMapView!
+    //Güncel Konumu almak için kullanıyoruz.
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        mapKit.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        
+    }
 }
 
