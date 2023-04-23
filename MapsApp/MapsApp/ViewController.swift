fe3c3ed880da0eaa0aecad3e91f83cf9975671b9
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-23 23:14:10 +0300

    region-span-location

--- MapsApp/MapsApp/ViewController.swift
+++ MapsApp/MapsApp/ViewController.swift
@@ -19,7 +19,10 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05 , longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location , span: span)
+        mapKit.setRegion(region, animated: true)
         
     }
 }
