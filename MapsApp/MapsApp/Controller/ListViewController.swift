803027368cb2e7619e8db70fb5c04c8292cc2822
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-24 12:09:19 +0300

    Updated.

--- MapsApp/MapsApp/Controller/ListViewController.swift
+++ MapsApp/MapsApp/Controller/ListViewController.swift
@@ -6,10 +6,14 @@
 //
 
 import UIKit
+import CoreData
 
 class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var isimDizisi = [String]()
+    var idDizisi = [UUID]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,8 +23,34 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(artiButtonTiklandi))
         navigationController?.navigationBar.topItem?.rightBarButtonItem?.tintColor = .white
-        
+        veriAl()
     }
+    func veriAl() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Yer")
+        request.returnsObjectsAsFaults = false
+        do {
+            let sonuclar = try context.fetch(request)
+            if sonuclar.count > 0 {
+                isimDizisi.removeAll(keepingCapacity: false)
+                idDizisi.removeAll(keepingCapacity: false)
+                for sonuc in sonuclar as! [NSManagedObject] {
+                    
+                    if let isim = sonuc.value(forKey: "isim") as? String {
+                        isimDizisi.append(isim)
+                    }
+                    if let id = sonuc.value(forKey: "id") as? UUID {
+                        idDizisi.append(id)
+                    }
+                }
+                tableView.reloadData()
+            }
+        }catch{
+            print("error!")
+        }
+    }
+    
     @objc func artiButtonTiklandi() {
         performSegue(withIdentifier: "toMapsVC", sender: nil)
     }
